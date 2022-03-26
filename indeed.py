85263071d8154a5922ae4ffc2e9a732dc771bf11
Paul Dawoon Kim <91786930+ITDawoon@users.noreply.github.com>
2022-03-26 19:16:29 +0000

    - Add Jobs' location and each url(ID) - Move the extracting jobs' information (title, company) function into the extract_job(html) function

--- indeed.py
+++ indeed.py
@@ -10,40 +10,49 @@ def extract_indeed_pages():
   pagination = soup.find("div", {"class": "pagination"})
   links = pagination.find_all('a')
   pages = []
-  
   for link in links[:-1]:
     pages.append(int(link.string))
-
   max_page = (pages[-1])
   return max_page
 
 
+def extract_job(html):
+  # Jobs' title
+  title = html.find("h2", {"class": "jobTitle"}).find("span", title=True).string
+
+  # Jobs' company
+  company = html.find("span", {"class": "companyName"})
+  company_anchor = company.find("a")
+  if company_anchor is not None:
+    company = str(company_anchor.string)
+  else:
+    company = str(company.string)
+  company = company.strip()
+
+  # Jobs' location
+  location = html.find("div", {"class": "companyLocation"}).text
+
+  # Jobs' ID
+  job_id = html["data-jk"]
+  return {
+    'title': title, 
+    'company': company, 
+    'location': location, 
+    "link": f"https://www.indeed.com/viewjob?jk={job_id}"}
+
+
 def extract_indeed_jobs(last_page):
   jobs = []
-  #for page in range(last _page):
-  result = requests.get(f"{URL}&start={0*LIMIT}")
-  soup = BeautifulSoup(result.text, "html.parser")
-
-  # Find all jobs
-  results = soup.find_all("a", {"class": "fs-unmask"})
-  print("")
+  for page in range(last_page):
+    print(f"Scrapping page {page}")
+    result = requests.get(f"{URL}&start={page*LIMIT}")
+    soup = BeautifulSoup(result.text, "html.parser")
   
-  for result in results:
-    # Jobs' title
-    jobTitle = result.find("h2", {"class": "jobTitle"})
-    title = jobTitle.find("span", title = True).string
-    if title == "new":
-      title = jobTitle.find_all("span")[1].string
-    #print(title)
-
-    # Jobs' company
-    company = result.find("span", {"class": "companyName"})
-    company_anchor = company.find("a")
-    if company_anchor is not None:
-      company = str(company_anchor.string)
-    else:
-      company = str(company.string)
-      company = company.strip()
-    #print(company)
-    print(title, company)
-  return jobs
+    # Find all jobs
+    results = soup.find_all("a", {"class": "fs-unmask"})
+  
+    # Append found job into "jobs" array
+    for result in results:
+      job = extract_job(result)
+      jobs.append(job)
+    return jobs
