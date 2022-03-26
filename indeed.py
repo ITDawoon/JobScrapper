61703573ce3d3ee4b9f21e2f0bf50710fa2655f0
Paul Dawoon Kim <91786930+ITDawoon@users.noreply.github.com>
2022-03-26 18:26:39 +0000

    - Add company extracting function which can filter the jobs that has no company (Filtering None values) - Make the whole code clearer

--- indeed.py
+++ indeed.py
@@ -10,25 +10,40 @@ def extract_indeed_pages():
   pagination = soup.find("div", {"class": "pagination"})
   links = pagination.find_all('a')
   pages = []
+  
   for link in links[:-1]:
     pages.append(int(link.string))
+
   max_page = (pages[-1])
   return max_page
 
 
 def extract_indeed_jobs(last_page):
   jobs = []
-  #for page in range(last_page):
+  #for page in range(last _page):
   result = requests.get(f"{URL}&start={0*LIMIT}")
   soup = BeautifulSoup(result.text, "html.parser")
+
+  # Find all jobs
   results = soup.find_all("a", {"class": "fs-unmask"})
   print("")
+  
   for result in results:
+    # Jobs' title
     jobTitle = result.find("h2", {"class": "jobTitle"})
-    title = jobTitle.find("span").string
-
+    title = jobTitle.find("span", title = True).string
     if title == "new":
       title = jobTitle.find_all("span")[1].string
-    
-    print(title)
+    #print(title)
+
+    # Jobs' company
+    company = result.find("span", {"class": "companyName"})
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+      company = str(company_anchor.string)
+    else:
+      company = str(company.string)
+      company = company.strip()
+    #print(company)
+    print(title, company)
   return jobs
